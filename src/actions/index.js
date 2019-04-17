2ab2abdfeb45905e198a0f4db3ca979eca1b0d0c
renegade27 <foemaster@live.com>
2019-04-16 21:24:41 -0500

    MVP I think don't quote me mate.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,32 @@
 // we'll need axios
-
+import axios from 'axios';
 // we'll need to create 3 different action types here.
 // one for fetching, one for success and one for failure
+
+export const FETCH_SUCCESS = 'FETCH_SUCCESS';
+export const FETCH_FAILURE = 'FETCH_FAILURE';
+export const FETCH_START = 'FETCH_START';
 
 // our action creator will be a function that returns a function
 // the url to fetch characters from is `https://swapi.co/api/people/`
 // remember that now we have controll over our thunk-based action creator
+
+export const fetch = () => dispatch => {
+    dispatch({
+        type : FETCH_START,
+        payload : true
+    })
+    const request = axios.get('https://swapi.co/api/people/');
+    request.then(({data}) => {
+        dispatch({
+            type : FETCH_SUCCESS,
+            payload : data.results
+        });
+    })
+    .catch(err => {
+        dispatch({
+            type : FETCH_FAILURE,
+            error : err
+        })
+    })
+}
--- src/reducers/starWarsReducer.js
+++ src/reducers/starWarsReducer.js
@@ -1,13 +1,31 @@
-import /* we need our action types here*/ "../actions";
+import { FETCH_START, FETCH_SUCCESS, FETCH_FAILURE } from "../actions";
+
 const initialState = {
-  characters: []
+  characters: [],
+  fetching: true,
+  error: ''
   // Array characters, Boolean fetching, null error.
 };
+
 export const charsReducer = (state = initialState, action) => {
   switch (action.type) {
-    // Fill me in with the important reducers
-    // action types should be FETCHING, SUCCESS and FAILURE
-    // your switch statement should handle all of these cases.
+    case FETCH_START:
+      return {
+        ...state,
+        fetching : true
+      }
+    case FETCH_SUCCESS:
+      return {
+        ...state,
+        fetching : false,
+        characters : action.payload
+      }
+    case FETCH_FAILURE:
+      console.log(action.error);
+      return {
+        ...state,
+        error : action.error
+      }
     default:
       return state;
   }
